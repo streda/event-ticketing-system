059cc0bca9aa1e10bd999e1d47793c4e7b941812
streda <samuelreda2018@gmail.com>
2025-04-25 16:22:30 -0700

    Managing database schema changes in development and production using database migrations

---
+++ services/auth-service/src/db/index.js
@@ -0,0 +1,27 @@
+// Basic PostgreSQL connection setup (reads DATABASE_URL from env).
+import pg from 'pg'; 
+
+const { Pool } = pg; 
+
+// Configuring the connection pool using environment variables
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    // May be I should Add SSL configuration for my database
+    // ssl: {
+    //   rejectUnauthorized: false 
+    // }
+});
+
+// Function to test the connection
+export const connectDB = async () => {
+    try {
+        await pool.connect();
+        console.log('Database connection pool established successfully.');
+    } catch (error) {
+        console.error('Error connecting to the database pool:', error);
+        throw error; // Re-throwing the error to be caught by the caller (server.js)
+    }
+};
+
+// Export the pool so that my controllers can use it for queries
+export default pool; 
